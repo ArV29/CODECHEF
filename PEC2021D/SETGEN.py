164cde81a91b507c4fb7b2c1df1a66d65a24bbe5
Aryan Raj Vaidya <aryanrajvaidya@gmail.com>
2021-02-20 14:54:49 +0530

    Update SETGEN.py

--- PEC2021D/SETGEN.py
+++ PEC2021D/SETGEN.py
@@ -4,15 +4,11 @@ def main():
     for i in range(t):
         n = int(input())
         
-        subset= []
-        for ii in range(1, n+1):
-            subset.append(ii)
-            print(ii)
-            for iii in range(0, 2**(ii-1)-1):
-                if type(subset[iii]) == int:
-                    subset.append([subset[iii]]+ [ii])
-                else:
-                    subset.append(subset[iii]+[ii])
+        subset= [[]]
+        for i in range(1, n+1):
+            
+            for ii in range(0, 2**(i-1)):
+                subset.append(subset[ii]+[i])
                 print(*subset[len(subset)-1])
         print()
 

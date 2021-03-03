2d84f4b23f308186b0525697e34c33b4bf572d2d
Aryan Raj Vaidya <aryanrajvaidya@gmail.com>
2021-03-03 15:19:08 +0530

    Update BNSRCH1.py

--- PEC2021F/BNSRCH1.py
+++ PEC2021F/BNSRCH1.py
@@ -1,10 +1,21 @@
-def run_program(x):
-    global cnt
-    global L
-    
-    cnt+=1
-    return x>=L
-
+import sys
+ 
+def run_program(m):
+    print('1 ' + str(m))
+    sys.stdout.flush()
+    r = int(input())
+    if r == 0:
+        return False
+    elif r == 1:
+        return True
+    exit()
+ 
+ 
+# ------------------- Do not touch anything above this line -------------------------------------
+ 
+# complete the function below, use run_program(x) to determine if Chef's program can consume x bytes
+# The result will be True if it can and False otherwise
+# If you call the function run_program more than 31 times, you will get a wrong answer verdict
 
 def find_memory_limit():
     low = 1
@@ -26,21 +37,6 @@ def find_memory_limit():
         return low
     else:
         return high
-
-
-def main():
-    global cnt
-    cnt = 0
-    global L
-    for L in reversed(range(1, 1000000000)):
-        calcL = find_memory_limit()
-        if L == calcL and cnt<=31:
-            print("For L:", L,"\n***** Success *****")
-        else:
-            print("Unsuccessfull at", L)
-            print("Given L:", L, "\t\tCalculated L", calcL, "\t\tTries", cnt)
-            break
-        cnt = 0
-
-
-main()
+ 
+# ------------------- Do not touch anything below this line -------------------------------------
+print("2 " + str(find_memory_limit()))

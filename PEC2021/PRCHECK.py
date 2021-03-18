c1c2e2dffe2e2d09c5383052d96aff04886fd980
Aryan Raj Vaidya <aryanrajvaidya@gmail.com>
2021-03-18 23:26:25 +0530

    Update PRCHECK.py

--- PEC2021/PRCHECK.py
+++ PEC2021/PRCHECK.py
@@ -3,9 +3,8 @@ if number == 1:
     print(0)
     exit()
 counter = 2
-while counter <= number**0.5:
+for counter in range(2, int(number**0.5)+1):
     if number % counter == 0:
         print(0)
         exit()
-    counter += 1
 print(1)

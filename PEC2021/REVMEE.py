09428bf805bb40d52d2d317c528f22b331e6cadf
Aryan Raj Vaidya <aryanrajvaidya@gmail.com>
2021-03-18 23:31:15 +0530

    Update REVMEE.py

--- PEC2021/REVMEE.py
+++ PEC2021/REVMEE.py
@@ -1,6 +1,8 @@
 length = int(input())
-length -= 1
+
 array = list(map(int, input().split()))
-while length >= 0:
-    print(array[length], end=" ")
-    length -= 1
+
+i = length -1
+while i >= 0:
+    print(array[i], end=" ")
+    i -= 1
